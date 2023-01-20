aec343976d638e67caf9d1e52824c3023c0b4784
Leonardo José Nunes <leonardo.ferax@gmail.com>
2023-01-20 17:01:03 -0300

    application.properties

---
+++ README.md
@@ -0,0 +1,6 @@
+# Springboot
+
+Arquivo [pom.xml](./pom.xml) com:
+
+- JPA
+- MySQL
---
+++ src/main/java/dio/aulamysql/AulaMysqlApplication.java
@@ -0,0 +1,13 @@
+package dio.aulamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaMysqlApplication.class, args);
+	}
+
+}
